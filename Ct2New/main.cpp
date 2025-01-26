50aead7c17bef326ce86e41cb3ee2aac4dd4f1f5
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-01-26 12:49:07 -0700

    Final Commit

--- Ct2New/main.cpp
+++ Ct2New/main.cpp
@@ -13,28 +13,30 @@
 int main() {
     std::string lastName;
     std::string firstName;
-    //FIXME change to 3
-    //numPatients is 3
-    int numPatients = 2;
+    //added this string to better format the output
+    std::string nameFormat = ", ";
+
+    int numPatients = 3;
+    
+    std::cout << "PATIENT REGISTRATION:\n";
     
     //create vector so we can get and print the user's input using a for loop
     std::vector<std::string> fullName(numPatients);
+    
     //get two string user inputs 3 (numPatients = 3) times
     for (int i = 0; i < numPatients; ++i) {
         //take two string inputs from user
+        std::cout << "Enter Patient " << i + 1 << "'s First Name: ";
+        std::getline(std::cin, firstName);
+        
         std::cout << "Enter Patient " << i + 1 << "'s Last Name: ";
         std::getline(std::cin, lastName);
         
-        
-        std::cout << "Enter Patient " << i + 1 << "'s First Name: ";
-        std::getline(std::cin, firstName);
-//FIXME should names be smooshed together?
-//FIXME would separating by comma not be concatenation?
-        //concatenate the two strings
-        fullName[i] = lastName + firstName;
+        //concatenate the strings
+        fullName[i] = lastName + nameFormat + firstName;
     }
 
-    std::cout << "\nPATIENT OUTPUT:\n";
+    std::cout << "\nPATIENTS REGISTERED:\n";
     //print concatenated inputs of various lengths
      for (int i = 0; i < numPatients; ++i) {
          //print concatenate of two entered strings as output onto screen
