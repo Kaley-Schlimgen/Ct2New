1d660e0368983088c2e225470fb095fff019470b
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-01-26 12:33:41 -0700

    Initial Commit

---
+++ Ct2New/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ct2New
+//
+//  Created by Kaley Schlimgen on 1/26/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
